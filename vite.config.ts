68cafa415a0ba977288910bfde1ffc89e25bdc67
Jonas Graterol <jonasgraterol@gmail.com>
2024-12-09 18:05:43 -0500

    build fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/web-2025/',
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
